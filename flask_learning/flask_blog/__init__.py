2639b23ea4aa9df2d3e0292356ec38de31e875cd
zhixianlu <827832075@qq.com>
2019-07-06 20:06:32 +0800

    version 4

---
+++ flask_learning/flask_blog/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e25bb31c88d1de7d77e961103c96e367' # import secrets -> secrets.token_hex(16)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db' #create the database in current location
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flask_blog import routes
+
